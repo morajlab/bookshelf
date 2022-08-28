f9815a526f0eeadceb20831356e6d0761bb6d202
Morteza Jamali <mortezajamali4241@gmail.com>
2022-08-28 00:52:43 +0000

    chore(packages): install `jest` for `google-drive-apps-script` package

---
+++ packages/google-drive-apps-script/jest.config.cjs
@@ -0,0 +1,11 @@
+/* eslint-disable */
+module.exports = {
+  displayName: 'google-drive-apps-script',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  coverageDirectory: '../../coverage/packages/google-drive-apps-script',
+};
