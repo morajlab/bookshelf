4102ab83563e97970a5dad96c8acdb22b84656fd
Morteza Jamali <mortezajamali4241@gmail.com>
2022-08-29 06:07:04 +0000

    chore(packages): complete `doGet()` function in `google-drive-apps-script` package

--- packages/google-drive-apps-script/src/api.ts
+++ packages/google-drive-apps-script/src/api.ts
@@ -181,6 +181,27 @@ class FileManager {
   };
 }
 
-const main = () => {
-  FileManager.arrangeDocuments();
+const EXPORTED_FUNCTIONS: { [k: string]: Function } = {
+  manageRootDirectories: FileManager.manageRootDirectories,
+  arrangeDocuments: FileManager.arrangeDocuments,
+  defaultFunction: () => ({
+    content: 'This is default function',
+  }),
+};
+
+const doGet = ({ parameter }: any) => {
+  const defaultParameter = {
+    function: 'defaultFunction',
+  };
+  const _parameter = { ...defaultParameter, ...parameter };
+  const functionName = EXPORTED_FUNCTIONS[_parameter.function]
+    ? EXPORTED_FUNCTIONS[_parameter.function]
+    : EXPORTED_FUNCTIONS[defaultParameter.function];
+  const output = ContentService.createTextOutput(
+    JSON.stringify(functionName())
+  );
+
+  output.setMimeType((MimeType as any).JSON);
+
+  return output;
 };
