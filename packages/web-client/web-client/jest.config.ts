4644377c15e8bfa498a404ec13c495dc25542bee
Morteza Jamali <mortezajamali4241@gmail.com>
2022-09-02 18:01:22 +0000

    feat(packages): create `web-client` app

---
+++ packages/web-client/web-client/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'web-client-web-client',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/packages/web-client/web-client',
+};
---
+++ packages/web-client/web-client/pages/index.tsx
@@ -0,0 +1,5 @@
+export const Index = () => {
+  return <div>Home Page</div>;
+};
+
+export default Index;
